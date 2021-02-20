57a651d69a0fbc16e2bfa364b7dede072239dcc9
Felix Schlitter <felixschlitter@gmail.com>
2021-02-20 19:03:12 +1300

    Implement missing C FFIs

--- src/Effect.c
+++ src/Effect.c
@@ -26,6 +26,45 @@ PURS_FFI_FUNC_3(Effect_bindE, _a, _f, _) {
 	return ret;
 }
 
+PURS_FFI_FUNC_2(Effect_untilE, _f, _) {
+	while (1) {
+		purs_any_t tmp = purs_any_app(_f, purs_any_null);
+		if (purs_any_eq(tmp, purs_any_true)) {
+			PURS_ANY_RELEASE(tmp);
+			break;
+		}
+		PURS_ANY_RELEASE(tmp);
+	}
+	return purs_any_null;
+}
+
+
+PURS_FFI_FUNC_3(Effect_whileE, _f, _a, _) {
+	while (1) {
+		purs_any_t tmp = purs_any_app(_f, purs_any_null);
+		if (purs_any_eq(tmp, purs_any_false)) {
+			PURS_ANY_RELEASE(tmp);
+			break;
+		}
+		PURS_ANY_RELEASE(tmp);
+		PURS_ANY_RELEASE(purs_any_app(_a, purs_any_null));
+	}
+	return purs_any_null;
+}
+
+PURS_FFI_FUNC_4(Effect_forE, _lo, _hi, _f, _) {
+	purs_int_t lo = purs_any_force_int(_lo);
+	purs_int_t hi = purs_any_force_int(_hi);
+	for (int i = lo; i < hi; i++) {
+		purs_any_t tmp1, tmp2;
+		tmp1 = purs_any_app(_f, purs_any_int(i));
+		tmp2 = purs_any_app(tmp1, purs_any_null);
+		PURS_ANY_RELEASE(tmp2);
+		PURS_ANY_RELEASE(tmp1);
+	}
+	return purs_any_null;
+}
+
 PURS_FFI_FUNC_3(Effect_foreachE, _as, f, _) {
 	int i;
 	purs_any_t a, tmp1, tmp2;
